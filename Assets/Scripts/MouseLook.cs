68c1abfe3fcdb57da66713022e74f8d53d999ed5
slevy2020 <slevy@chadwickschool.org>
2019-09-05 14:52:08 -0700

    feat: add movement and camera
    this should've been a few commits whoops

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+  public enum RotationAxes {
+    MouseXAndY = 0,
+    MouseX = 1,
+    MouseY = 2
+  }
+  public RotationAxes axes = RotationAxes.MouseXAndY;
+
+  public float sensitivityHor = 9.0f;
+  public float sensitivityVert = 9.0f;
+
+  public float minVert = -45.0f;
+  public float maxVert = 45.0f;
+
+  private float _rotationX = 0;
+
+  void Start() {
+    Rigidbody body = GetComponent<Rigidbody>();
+    if (body != null) {
+      body.freezeRotation = true;
+    }
+  }
+
+  void Update() {
+    if (axes == RotationAxes.MouseX) {
+      transform.Rotate(0, Input.GetAxis("Mouse X")*sensitivityHor, 0);
+    } else if (axes == RotationAxes.MouseY) {
+      _rotationX -= Input.GetAxis("Mouse Y")*sensitivityVert;
+      _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+      float rotationY = transform.localEulerAngles.y;
+
+      transform.localEulerAngles = new Vector3(_rotationX, rotationY);
+    } else {
+      _rotationX -= Input.GetAxis("Mouse Y")*sensitivityVert;
+      _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+      float delta = Input.GetAxis("Mouse X")*sensitivityHor;
+      float rotationY = transform.localEulerAngles.y + delta;
+
+      transform.localEulerAngles = new Vector3(_rotationX, rotationY);
+    }
+  }
+}
--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spin : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Spin : MonoBehaviour {
+  public float speed = 3.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void Update() {
+    transform.Rotate(0, speed, 0);
+  }
 }
